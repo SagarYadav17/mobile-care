47bba2ed23b9855eebe2c4350951b5f8478d8cbd
Sagar <sy938822@gmail.com>
2020-09-03 13:30:23 +0530

    facebook login test

--- mobile_care/settings.py
+++ mobile_care/settings.py
@@ -182,7 +182,6 @@ SOCIALACCOUNT_PROVIDERS = {
             'email'
         ],
         'EXCHANGE_TOKEN': True,
-        'LOCALE_FUNC': 'path.to.callable',
         'VERIFIED_EMAIL': False,
         'VERSION': 'v2.8'}
 }
